ad230680207acd93b534fc52159bc6cc479d7ec3
hashk1 <7278427+hashk1@users.noreply.github.com>
2020-11-22 15:35:34 +0900

    Modifiy es2juli.cr

--- es2juli.cr
+++ es2juli.cr
@@ -27,8 +27,133 @@ def usage
 end
 
 def convert(line, option = nil) 
-  es = "ABCDEFGHIJKLMNOPRSTUVZabcdefghijklmnoprstuvz\u0108\u0109\u011C\u011D\u0124\u0125\u0134\u0135\u015C\u015D\u016C\u016D!?\\"
-  juli = "\uFF21\uFF22\uFF23\uFF24\uFF25\uFF26\uFF27\uFF28\uFF29\uFF2A\uFF2B\uFF2C\uFF2D\uFF2E\uFF2F\uFF30\uFF32\uFF33\uFF34\uFF35\uFF36\uFF3A\uFF41\uFF42\uFF43\uFF44\uFF45\uFF46\uFF47\uFF48\uFF49\uFF4A\uFF4B\uFF4C\uFF4D\uFF4E\uFF4F\uFF50\uFF52\uFF53\uFF54\uFF55\uFF56\uFF5A\u3322\u334D\u3318\u3314\u3336\u3303\u333B\u330D\u3357\u332B\u3327\u3326\u3349\u3351\u334A"
+
+  es_list = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "z",
+    "\u0108", # Ĉ
+    "\u0109", # ĉ
+    "\u011c", # Ĝ
+    "\u011d", # ĝ
+    "\u0124", # Ĥ
+    "\u0125", # ĥ
+    "\u0134", # Ĵ
+    "\u0135", # ĵ
+    "\u015c", # Ŝ
+    "\u015d", # ŝ
+    "\u016c", # Ŭ
+    "\u016d", # ŭ
+    "!",
+    "?",
+    "\\"
+  ]
+    
+  juli_list = [
+    "\uff21", # Ａ
+    "\uff22", # Ｂ
+    "\uff23", # Ｃ
+    "\uff24", # Ｄ
+    "\uff25", # Ｅ
+    "\uff26", # Ｆ
+    "\uff27", # Ｇ
+    "\uff28", # Ｈ
+    "\uff29", # Ｉ
+    "\uff2a", # Ｊ
+    "\uff2b", # Ｋ
+    "\uff2c", # Ｌ
+    "\uff2d", # Ｍ
+    "\uff2e", # Ｎ
+    "\uff2f", # Ｏ
+    "\uff30", # Ｐ
+    "\uff32", # Ｒ
+    "\uff33", # Ｓ
+    "\uff34", # Ｔ
+    "\uff35", # Ｕ
+    "\uff36", # Ｖ
+    "\uff3a", # Ｚ
+    "\uff41", # ａ
+    "\uff42", # ｂ
+    "\uff43", # ｃ
+    "\uff44", # ｄ
+    "\uff45", # ｅ
+    "\uff46", # ｆ
+    "\uff47", # ｇ
+    "\uff48", # ｈ
+    "\uff49", # ｉ
+    "\uff4a", # ｊ
+    "\uff4b", # ｋ
+    "\uff4c", # ｌ
+    "\uff4d", # ｍ
+    "\uff4e", # ｎ
+    "\uff4f", # ｏ
+    "\uff50", # ｐ
+    "\uff52", # ｒ
+    "\uff53", # ｓ
+    "\uff54", # ｔ
+    "\uff55", # ｕ
+    "\uff56", # ｖ
+    "\uff5a", # ｚ
+    "\u3322", # ㌢
+    "\u334d", # ㍍
+    "\u3318", # ㌘
+    "\u3314", # ㌔
+    "\u3336", # ㌶
+    "\u3303", # ㌃
+    "\u333b", # ㌻
+    "\u330d", # ㌍
+    "\u3357", # ㍗
+    "\u332b", # ㌫
+    "\u3327", # ㌧
+    "\u3326", # ㌦
+    "\u3349", # ㍉
+    "\u3351", # ㍑
+    "\u334a", # ㍊
+  ]
+
+  es = es_list.join("")
+  juli = juli_list.join("")
 
   if option == "-e"
     return line.tr(juli, es)
